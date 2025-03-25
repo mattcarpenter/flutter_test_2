f598e820e795ac0418f58fe5fe1406ddf49aa1ca
Matt Carpenter <mattcarpenter@gmail.com>
2025-03-25 19:31:23 +0900

    fix android build

---
+++ android/app/src/main/kotlin/com/mattcarpenter/recipe_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.mattcarpenter.recipe_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
--- android/app/src/main/kotlin/net/mattcarpenter/flutter_test_2/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package net.mattcarpenter.flutter_test_2
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
