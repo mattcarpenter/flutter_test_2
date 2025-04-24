ed5427aea78a69556e8f969616d85190de59b281
Matt Carpenter <mattcarpenter@gmail.com>
2025-04-24 22:22:13 +0900

    rename library show name

--- lib/database/custom_open_factory.dart
+++ lib/database/custom_open_factory.dart
@@ -3,7 +3,7 @@ import 'dart:ffi' show DynamicLibrary;
 import 'package:powersync/powersync.dart';
 import 'package:powersync/sqlite3_common.dart';
 import 'package:powersync/sqlite_async.dart';
-import 'package:sqlite3/src/ffi/load_library.dart' as loadLibraryOpen;
+import 'package:sqlite3/src/ffi/load_library.dart' as load_library_open;
 
 class CustomOpenFactory extends PowerSyncOpenFactory {
   CustomOpenFactory({required super.path, super.sqliteOptions });
@@ -21,7 +21,7 @@ class CustomOpenFactoryForTest extends PowerSyncOpenFactory {
 
   @override
   CommonDatabase open(SqliteOpenOptions options) {
-    loadLibraryOpen.open.overrideForAll(() => DynamicLibrary.open('/Users/matt/repos/sqlite-src-3490100/libsqlite3.dylib'));
+    load_library_open.open.overrideForAll(() => DynamicLibrary.open('/Users/matt/repos/sqlite-src-3490100/libsqlite3.dylib'));
     final db = super.open(options);
     // Define UDF functions here
     return db;
