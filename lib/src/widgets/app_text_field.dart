3a78fa3d99087addad993110b067fcbe1b65fc07
Matt Carpenter <mattcarpenter@gmail.com>
2025-07-21 07:54:05 +0900

    weight

--- lib/src/widgets/app_text_field.dart
+++ lib/src/widgets/app_text_field.dart
@@ -311,6 +311,7 @@ class _AppTextFieldState extends State<AppTextField>
                           maxLength: widget.maxLength,
                           style: TextStyle(
                             fontSize: 16.0,
+                            fontWeight: FontWeight.w600,
                             color: widget.enabled ? Colors.black : _disabledColor,
                             height: 1.5,
                           ),
